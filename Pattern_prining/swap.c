99b9fcb1633dda0449b5f917f426e5ccb487781e
chiranthcs06 <chiranthcs5@gmail.com>
2025-02-23 19:56:09 +0530

    abc

---
+++ Pattern_prining/swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+  int a;
+  int b;
+  printf("Enter the value of A \n");
+  scanf("%d",&a);
+  printf("Enter the value of B \n");
+  scanf("%d",&b);
+  a=a^b;
+  b=b^a;
+  a=a^b;
+  printf("A: %d\n",a);
+  printf("B: %d\n",b);
+  return 0;
+}
